d82a20a90484b3096507e590ba30ea5c85b10daa
Carlos Tonatihu Barrera <carlostonatihu@gmail.com>
2015-05-21 09:32:47 -0500

    Tratando de agregar el onclick al menu

--- app/src/main/java/com/strom/strongfit/adapters/CustomRecyclerViewAdapter.java
+++ app/src/main/java/com/strom/strongfit/adapters/CustomRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.strom.strongfit.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.strom.strongfit.R;
 
@@ -23,25 +25,27 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
     private static final int TYPE_HEADER = 0;
     private static final int TYPE_ITEM = 1;
     static final String TAG = CustomRecyclerViewAdapter.class.getSimpleName();
+    private Context _context;
     //Primero recivimos los datos
-    public CustomRecyclerViewAdapter(String[] titles, String nombre, String peso, int perfilImagen, int[] iconos) {
+    public CustomRecyclerViewAdapter(String[] titles, String nombre, String peso, int perfilImagen, int[] iconos, Context mContext) {
         this.titles = titles;
         this.nombre = nombre;
         this.peso = peso;
         this.perfilImagen = perfilImagen;
         this.iconos = iconos;
+        this._context = mContext;
     }
     //Este metodo se ejecuta para crear la vista
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         if (i == TYPE_ITEM) {
             View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.drawer_list_item, viewGroup, false); //Infla la lista
-            ViewHolder vhItem = new ViewHolder(view, i); //Le pasamos los valores a nuestro Holder (la vista y el tipo)
+            ViewHolder vhItem = new ViewHolder(view, i, _context); //Le pasamos los valores a nuestro Holder (la vista y el tipo)
 
             return vhItem;
         } else if (i == TYPE_HEADER) {
             View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.header_paciente, viewGroup, false); //Infla el header
-            ViewHolder vhHeader = new ViewHolder(view, i);
+            ViewHolder vhHeader = new ViewHolder(view, i, _context);
 
             return vhHeader;
         }
@@ -79,16 +83,18 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
         return position == TYPE_HEADER;
     }
     //Usamos un Holder para solo inicializar una vez cada elemento
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         int holderID;
         private TextView nombre_text;
         private TextView peso_text;
         private TextView drawer_item_text;
         private ImageView profile_image;
         private ImageView drawer_item_icon;
+        private Context contexto;
 
-        public ViewHolder(View itemView, int viewType) {
+        public ViewHolder(View itemView, int viewType, Context c) {
             super(itemView);
+            this.contexto = c;
             if(viewType == TYPE_ITEM){
                 drawer_item_text = (TextView) itemView.findViewById(R.id.drawer_item_text);
                 drawer_item_icon = (ImageView) itemView.findViewById(R.id.drawer_item_icon);
@@ -99,6 +105,11 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
                 profile_image = (ImageView) itemView.findViewById(R.id.profile_image);
                 holderID = 0;
             }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(contexto, "Apretaste: " + getPosition(), Toast.LENGTH_SHORT).show();
         }
     }
 }
