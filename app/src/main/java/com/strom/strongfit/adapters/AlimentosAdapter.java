5f7e1052ba01abf472d82324a0e2f0e83697f47a
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-05-21 19:56:28 -0500

    Ya muestra los alimentos

--- app/src/main/java/com/strom/strongfit/adapters/AlimentosAdapter.java
+++ app/src/main/java/com/strom/strongfit/adapters/AlimentosAdapter.java
@@ -5,10 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.strom.strongfit.R;
 import com.strom.strongfit.models.Alimento;
