51ccf7785fb90ee9d8f3f68cfb244b4853c39230
Carlos Tonatihu Barrera <carlostonatihu@gmail.com>
2015-06-12 13:54:46 -0500

    Conexion para el inicio de sesion

--- app/src/main/java/com/strom/strongfit/LoginActivity.java
+++ app/src/main/java/com/strom/strongfit/LoginActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.strom.strongfit.utils.ConectarHTTP;
 import com.strom.strongfit.utils.SessionManager;
 
 import java.io.BufferedReader;
@@ -45,77 +46,47 @@ public class LoginActivity extends ActionBarActivity {
         }
         input_email = (EditText) findViewById(R.id.input_email);
         input_password = (EditText) findViewById(R.id.input_password);
+        ConectarHTTP conectarHTTP = new ConectarHTTP();
     }
 
     public void onClickEntrar(View view) {
         email = input_email.getText().toString();
         password = input_password.getText().toString();
         Intent intent;
-        new GetHoraTask().execute();
-
-        if (email.equals("paciente@gmail.com") && password.equals("123")) {
-            sessionManager.createLogInSession("paciente@gmail.com", "Moy", "paciente");
-            intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            this.finish();
-        }else {
-            Toast.makeText(this, "paciente@gmail.com o medico@gmail.com y 123", Toast.LENGTH_SHORT).show();
-        }
+        new IniciarSesionTask().execute();
     }
     public void onClickRegistro(View view){
         Intent intent = new Intent(this, RegistroActivity.class);
         startActivity(intent);
     }
 
-    public class GetHoraTask extends AsyncTask<Object, Void, String> {
+    public class IniciarSesionTask extends AsyncTask<Object, Void, String> {
 
         @Override
         protected String doInBackground(Object... params) {
             ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
-
-                try {
-                    Log.d(TAG, "Entro al try");
-                    //No olviden cambiar la ip
-                    URL url = new URL("http://192.168.1.141:8080/StrongFit/sLoginAndroid");
-                    httpConnection = (HttpURLConnection) url.openConnection();
-                    httpConnection.setRequestMethod("POST");
-                    httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                    httpConnection.setDoOutput(true);
-                    String postParameters = "correo=value1&contra=value2";
-                    httpConnection.setFixedLengthStreamingMode(
-                            postParameters.getBytes().length);
-                    PrintWriter out = new PrintWriter(httpConnection.getOutputStream());
-                    out.print(postParameters);
-                    out.close();
-                    int responseCode = httpConnection.getResponseCode();
-                    Log.w(TAG, String.valueOf(responseCode));
-
-
-                    bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
-                    String linea;
-                    response = new StringBuilder();
-                    while((linea = bufferedReader.readLine()) != null){
-                        response.append(linea);
-                    }
-                    Log.i(TAG, "Valor: " + response.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Log.e(TAG, e.getMessage());
-                }finally {
-                    if (httpConnection != null) {
-                        httpConnection.disconnect();
-                    }
-                }
+                String respuesta = conectarHTTP.iniciarSesion();
+                return respuesta;
             } else {
                 Log.e(TAG, "No hay red");
             }
-            return "Salio bueno";
+            return "No se encontraron datos";
         }
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
+            if (s.equals("entra")) {
+                sessionManager.createLogInSession("paciente@gmail.com", "Moy", "paciente");
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                this.finish();
+            }else {
+                Toast.makeText(this, "paciente@gmail.com o medico@gmail.com y 123", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
